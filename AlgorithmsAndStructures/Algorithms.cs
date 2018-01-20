8dbbb5cfe43de41f49d70f291048f21acedbfbb7
Alexandr <sasha732sasha000@gmail.com>
2018-01-20 16:44:38 +0200

    Added simple searches, binary search and KMP search

---
+++ AlgorithmsAndStructures/Algorithms.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace AlgorithmsAndStructures
+{
+    public static class Algorithms<T> where T : IComparable
+    {
+        #region Search
+
+        public static int SimpleSeaarch( T[] array, T element)
+        {
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index].Equals(element))
+                    return index;
+            }
+
+            return -1;
+        }
+        
+        public static int BinarySearch( T[] sortedArray, T element ){
+            int mid = sortedArray.Length / 2;
+            int lo = 0;
+            int hi = sortedArray.Length;
+            T currentElement = sortedArray[mid];
+            int counter = 0;
+
+            while(!currentElement.Equals(element)){
+                currentElement = sortedArray[mid];
+
+                if(currentElement.CompareTo(element) < 0){
+                    lo = mid;
+                    mid = (hi + lo) / 2;
+                }else if(currentElement.CompareTo(element) > 0)
+                {
+                    hi = mid;
+                    mid = (hi + lo) / 2;
+                }
+
+                if (counter > Math.Log(sortedArray.Length))
+                    return -1;
+                
+                counter++;
+            }
+            return mid;
+        }
+
+        #endregion
+        
+        
+    }
+}
---
+++ AlgorithmsAndStructures/StringAlgorithms.cs
@@ -0,0 +1,87 @@
+﻿namespace AlgorithmsAndStructures
+{
+    public static class StringAlgorithms
+    {
+        #region Search
+
+        public static int DirectSearch(string str, string subStr)
+        {
+            int index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == subStr[index])
+                {
+                    index++;
+                    if (index == subStr.Length - 1)
+                        return i - index;
+                }
+                else
+                {
+                    index = 0;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int KMPSearch(string text, string subStr)
+        {
+            int[] prefixes = prefixKMP(subStr);
+            int k = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                while (k > 0 && text[i] != subStr[k])
+                {
+                    k = prefixes[k - 1];
+                }
+
+                if (subStr[k] == text[i])
+                {
+                    k++;
+                }
+
+                if (k == subStr.Length - 1)
+                {
+                    return i - subStr.Length + 1;
+                }
+                
+            }
+
+            return -1;
+        }
+
+        #endregion
+        
+        
+
+        #region Helpers
+
+        public static int[] prefixKMP(string subString)
+        {
+            int lenght = subString.Length;
+            int[] prefixes = new int[lenght];
+            prefixes[0] = 0;
+
+            for (int i = 1; i < lenght; i++)
+            {
+                int k = prefixes[i - 1];
+
+                while (k > 0 && subString[i] != subString[k])
+                {
+                    k = prefixes[k - 1];
+                }
+
+                if (subString[i] == subString[k])
+                {
+                    k++;
+                }
+
+                prefixes[i] = k;
+            }
+
+            return prefixes;
+        }
+
+        #endregion
+    }
+}
